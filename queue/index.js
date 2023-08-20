92ec1d2a474dda40135e5289219213d4433a9ee1
penguin <hjh6195@naver.com>
2023-08-21 03:21:41 +0900

    removed legacy codes

--- queue/index.js
+++ queue/index.js
@@ -67,7 +67,7 @@ app.post('/quest', function (req, res) {
   }
 
   const id = req.body.id;
-  if (!token) {
+  if (!id) {
     res.sendStatus(500);
     return;
   }
@@ -77,68 +77,20 @@ app.post('/quest', function (req, res) {
     return;
   }
 
+  let question = req.body.question;
+  if (!question) {
+    res.sendStatus(500);
+    return;
+  }
+
   res.json({
-    answer: 'congratualations!'
+    answer: question + question
   });
-});
-
-
-app.get('/', function (req, res) {
-  res.sendFile( __dirname + "/pages/" + "index.html" );
 });
 
 
 app.get('/hello', function (req, res) {
   res.send('Hello, imagpt');
-});
-
-
-let id = 0;
-function GetId() {
-  let res = id;
-  id++;
-  return res;
-}
-
-const waitings = {};
-
-app.post('/quest_old', function(req, res) {
-  let question = req.body.question;
-  let id = GetId();
-
-  waitings[id] = { 
-    question: question
-  };
-
-  res.json({
-    id: id
-  });
-});
-
-
-app.post('/image', function(req, res) {
-  const { image } = req.files;
-  const { id } = req.body;
-
-  if (!image) {
-    return res.sendStatus(400);
-  }
-
-  let ext = image.name.split('.');
-  ext = ext[ext.length - 1];
-  image_name = String(id) + '.' + ext;
-
-  image.mv(__dirname + '/../images/' + image_name, (err) => {
-    if (err) {
-      return res.sendStatus(400);
-    }
-
-    processors[0].Quest(image_name, waitings[id].question, (data) => {
-      res.json({
-        answer: data
-      });
-    });
-  });
 });
 
 
